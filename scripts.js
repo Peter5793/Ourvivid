2e4d03c83d7fce714d8fb26c112dc404ac25cbc6
Peter Lugalia <p.lugalia@launchpad.alx.app>
2021-12-10 00:35:36 +0100

    add post requst in form

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-const scriptURL = 'https://script.google.com/home/projects/1CM0ZtPcZae87pqnB1SE9ByK-w84wzQR91bwEKz7MZBiNQGi8KzFgiYis/edit'
-            const form = document.forms['google-sheet']
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwoVaI2YcXVoGeJ9VRB1YK80aIi-UJIpB__zOS5dp06WvNRlKGPwvYw0b04t0nSl49Z/exec'
+const form = document.forms['google-sheet']
           
             form.addEventListener('submit', e => {
               e.preventDefault()
               fetch(scriptURL, { method: 'POST', body: new FormData(form)})
                 .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
                 .catch(error => console.error('Error!', error.message))
-            })
+})
